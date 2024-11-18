6384ff5e3867455d5f10c256bbd3518929590d45
eeperfume <eeperfume@naver.com>
2024-11-18 14:22:10 +0900

    first commit

---
+++ kotlin-spring-backend-practice/src/main/kotlin/com/example/practice/KotlinSpringBackendPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBackendPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBackendPracticeApplication>(*args)
+}
